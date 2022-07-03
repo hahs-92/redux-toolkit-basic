50e5f613485017a1dddba6a07862fce5afc39406
hahs <alexandre_2420@outlook.com>
2022-07-03 16:19:16 -0500

    [added]: getTodoById functionality

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+//rtk hooks
+import { useGetTodosQuery, useGetTodoByIdQuery } from "./store/apis/todosApi";
 
 export default function TodoApp() {
+  const [todoId, setTodoId] = useState(1);
+  //   const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoByIdQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId == 1) return;
+    setTodoId(todoId - 1);
+  };
+
   return (
     <main>
       <h1>Rtk</h1>
       <hr />
+      {isLoading && <span>Loading...</span>}
+      {!isLoading && <pre>{JSON.stringify(todo)}</pre>}
+
+      <button onClick={nextTodo}>Next</button>
+      <button onClick={prevTodo}>Prev</button>
+
+      {/* <section>
+        <ul>
+          {!isLoading &&
+            todos.length &&
+            todos.map((todo) => <li key={todo.id}>{todo.title}</li>)}
+        </ul>
+      </section> */}
     </main>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 //reducers
 import { counterSlice } from "./slices/counter";
 import { pokemonSlice } from "./slices/pokemon";
+//RTK
+import { todosApi } from "./apis/todosApi";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemons: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
