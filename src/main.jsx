6f448041825aedf3f599fcc1e8f58d47ac653dc6
hahs <alexandre_2420@outlook.com>
2022-07-03 14:45:06 -0500

    [added]: axios implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+//screens
+import App from "./App";
+import PokemonApp from "./PokemonApp";
+import "./index.css";
 //redux
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 //store
-import {  store } from './store/store'
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={ store }>
-      <App />
+    <Provider store={store}>
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
-)
+);
--- src/store/slices/todos/thunks.js
+++ src/store/slices/todos/thunks.js
@@ -1,11 +1,16 @@
 //slices
 import { startLoadingPokemons, setPokemons } from "./pokemonSlice";
+//api
+import { pokemonApi } from "../../../api/pokemonApi";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    //TODO: Realizar peticion http
-    //dispatch(setPokemons())
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10offset=${page * 10}`
+    );
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
